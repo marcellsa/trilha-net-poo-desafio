597b96c74559102d7347ff3d19f1ad5a15a06b2c
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-01-03 17:02:40 -0300

    feat: método InstalarAplicativo implementado

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -5,8 +5,12 @@ namespace DesafioPOO.Models
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
-            
+
         }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string aplicativo)
+        {
+            Console.WriteLine($"Instalando {aplicativo} no Nokia");
+        }
     }
 }
