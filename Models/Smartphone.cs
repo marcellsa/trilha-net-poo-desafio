0fb814fe088a41de9b36886081631cf3e26af57e
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-01-03 16:44:58 -0300

    feat: adicionado propriedades em Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,8 +4,10 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
