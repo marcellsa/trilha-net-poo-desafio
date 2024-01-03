97c7ad48f2c49837031422d10404b826327f7f53
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-01-03 16:47:57 -0300

    feat: adicionado parâmentros no construtor da class  Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -7,10 +7,14 @@ namespace DesafioPOO.Models
         private string Modelo { get; set; }
         private string IMEI { get; set; }
         private int Memoria { get; set; }
+        
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
